22cc22d3a9574fd87a8ae19d1e8f158d37f23592
romanbrancato <89923118+romanbrancato@users.noreply.github.com>
2023-04-18 20:11:11 -0400

    App.vue finished

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,56 @@
 
 <template>
   <div>
-    <h1>ROMAN BRANCATO</h1>
+    <img class="logo" src="./assets/logo.png">
+  </div>
+  <div class="link-container">
+    <RouterLink to="/login">
+      <fa :icon="['fass', 'tags']" /> List Item
+    </RouterLink>
+
+    <RouterLink to="/login">
+      <fa :icon="['fas', 'user']" /> Login
+    </RouterLink>
+
+    <RouterLink to="/">
+      <fa :icon="['fas', 'handshake-angle']" /> Help
+    </RouterLink>
+
+  </div>
+  <div class="center-menu">
+    <router-view></router-view>
   </div>
 </template>
+
+<style scoped>
+.center-menu {
+  position: fixed;
+  top: 13%;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 150vh;
+  height: 85vh;
+  background-color: white;
+  border-radius: 40px;
+  overflow: auto;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+  background: linear-gradient(to bottom right, #97BEB9, White);
+}
+
+.logo {
+  width: 110px;
+  height: 120px;
+  position: fixed;
+  left: 13.6%;
+  top: 6.5%;
+  transform: translateY(-50%);
+}
+
+.link-container{
+  position: fixed;
+  top: 6%;
+  right: 10%;
+  width: 50vh;
+  height: 10vh;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,40 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+import { createRouter, createWebHashHistory } from "vue-router"
+import Home from "./components/Home.vue"
+import Login from "./components/Login.vue"
+import SignUp from "./components/SignUp.vue"
+import Profile from "./components/Profile.vue"
+import Statistics from "./components/Statistics.vue"
+import CreateOffer from "./components/CreateOffer.vue"
+import Item from "./components/Item.vue"
+import Help from "./components/Help.vue"
+
+
+const componentRoutes = [
+    { path: "/", component: Home },
+    { path: "/login", component: Login },
+    { path: "/signup", component: SignUp },
+    { path: "/profile", component: Profile },
+    { path: "/help", component: Help },
+    { path: "/statistics", component: Statistics },
+    { path: "/createoffer", component: CreateOffer },
+    { path: "/item", component: Item },
+];
+
+const router = createRouter({
+    routes: componentRoutes,
+    history: createWebHashHistory()
+});
+
+library.add(fas);
+
+createApp(App)
+    .use(router)
+    .component('fa', FontAwesomeIcon)
+    .mount('#app')
